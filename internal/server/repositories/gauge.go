e2fc39340fa71a7ecd9ac17c2e51b7fcddd0a95a
Лапшин Иван Александрович <ilapshin@phoenixit.ru>
2025-04-10 17:39:59 +0300

    iter21 добавл абстракцию над бд

--- internal/server/repositories/gauge.go
+++ internal/server/repositories/gauge.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
+	"fmt"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/vysogota0399/mem_stats_monitoring/internal/server/models"
@@ -29,7 +30,7 @@ func NewGauge(strg storage.Storage, lg *logging.ZapLogger) *Gauge {
 
 // Create сохраняет новую запись в хранилище.
 func (g *Gauge) Create(ctx context.Context, record *models.Gauge) (*models.Gauge, error) {
-	if s, ok := g.storage.(storage.DBAble); ok {
+	if s, ok := g.storage.(DBAble); ok {
 		return g.pushToDB(ctx, s, record)
 	}
 
@@ -40,17 +41,19 @@ func (g *Gauge) Create(ctx context.Context, record *models.Gauge) (*models.Gauge
 	return record, nil
 }
 
-func (g *Gauge) pushToDB(ctx context.Context, s storage.DBAble, r *models.Gauge) (*models.Gauge, error) {
-	if err := s.DB().QueryRowContext(
+func (g *Gauge) pushToDB(ctx context.Context, s DBAble, r *models.Gauge) (*models.Gauge, error) {
+	if err := s.QueryRowContext(
 		ctx,
 		`
-			insert into gauges(name, value)
-			values ($1, $2)
-			returning id
+			INSERT INTO gauges(name, value)
+			VALUES ($1, $2)
+			RETURNING id
 		`,
-		r.Name,
-		r.Value,
-	).Scan(&r.ID); err != nil {
+		[]any{r.Name, r.Value},
+		func(rows *sql.Rows) error {
+			return rows.Scan(&r.ID)
+		},
+	); err != nil {
 		return nil, err
 	}
 
@@ -59,30 +62,30 @@ func (g *Gauge) pushToDB(ctx context.Context, s storage.DBAble, r *models.Gauge)
 
 // Last возвращает последнюю запись из хранилища.
 func (g Gauge) Last(ctx context.Context, mName string) (*models.Gauge, error) {
-	if s, ok := g.storage.(storage.DBAble); ok {
+	if s, ok := g.storage.(DBAble); ok {
 		return g.lastFromDB(ctx, s, mName)
 	}
 
 	return g.lastFromMem(mName)
 }
 
-func (g Gauge) lastFromDB(ctx context.Context, s storage.DBAble, mName string) (*models.Gauge, error) {
-	row := s.DB().QueryRowContext(
+func (g Gauge) lastFromDB(ctx context.Context, s DBAble, mName string) (*models.Gauge, error) {
+	gg := &models.Gauge{Name: mName}
+
+	if err := s.QueryRowContext(
 		ctx,
 		`
-		select value, id
-		from gauges
-		where name = $1
-		order by id desc
-		limit 1`, mName)
-
-	gg := &models.Gauge{Name: mName}
-	if err := row.Scan(&gg.Value, &gg.ID); err != nil {
-		if err == sql.ErrNoRows {
-			return nil, storage.ErrNoRecords
-		} else {
-			return nil, err
-		}
+		SELECT value, id
+		FROM gauges
+		WHERE name = $1
+		ORDER BY id DESC
+		LIMIT 1`,
+		[]any{mName},
+		func(rows *sql.Rows) error {
+			return rows.Scan(&gg.Value, &gg.ID)
+		},
+	); err != nil {
+		return nil, fmt.Errorf("internal/server/repositories/gauge.go: query row context error %w", err)
 	}
 
 	return gg, nil
@@ -128,43 +131,44 @@ func (g Gauge) All() map[string][]models.Gauge { //nolint:dupl // :/
 
 // SaveCollection сохраняет пачку записей в хранилище
 func (g *Gauge) SaveCollection(ctx context.Context, coll []models.Gauge) ([]models.Gauge, error) {
-	if s, ok := g.storage.(storage.DBAble); ok {
+	if s, ok := g.storage.(DBAble); ok {
 		return g.saveCollToDB(ctx, s, coll)
 	}
 
 	return g.saveCollToMem(coll)
 }
 
-func (g *Gauge) saveCollToDB(ctx context.Context, s storage.DBAble, coll []models.Gauge) ([]models.Gauge, error) {
-	tx, err := s.DB().BeginTx(ctx, &sql.TxOptions{})
+func (g *Gauge) saveCollToDB(ctx context.Context, s DBAble, coll []models.Gauge) ([]models.Gauge, error) {
+	tx, err := s.BeginTx(ctx, &sql.TxOptions{})
 	if err != nil {
 		return nil, err
 	}
 
 	defer func() {
-		if commitErr := tx.Commit(); commitErr != nil {
-			if rollbackErr := tx.Rollback(); rollbackErr != nil {
+		if commitErr := s.CommitTx(ctx, tx); commitErr != nil {
+			if rollbackErr := s.RollbackTx(ctx, tx); rollbackErr != nil {
 				g.lg.ErrorCtx(ctx, "failed to rollback transaction after commit error", zap.Error(rollbackErr))
 			}
 		}
 	}()
 
 	for _, rec := range coll {
-		res := tx.QueryRowContext(
+		if err := s.QueryRowContext(
 			ctx,
 			`
-			insert into gauges(name, value)
-			values ($1, $2)
-			returning id
+			INSERT INTO gauges(name, value)
+			VALUES ($1, $2)
+			RETURNING id
 			`,
-			rec.Name,
-			rec.Value,
-		)
-		if err := res.Scan(&rec.ID); err != nil {
-			if rollbackErr := tx.Rollback(); rollbackErr != nil {
+			[]any{rec.Name, rec.Value},
+			func(rows *sql.Rows) error {
+				return rows.Scan(&rec.ID)
+			},
+		); err != nil {
+			if rollbackErr := s.RollbackTx(ctx, tx); rollbackErr != nil {
 				g.lg.ErrorCtx(ctx, "failed to rollback transaction after scan error", zap.Error(rollbackErr))
 			}
-			return nil, err
+			return nil, fmt.Errorf("internal/server/repositories/gauge.go: query row context error %w", err)
 		}
 	}
 
@@ -173,7 +177,7 @@ func (g *Gauge) saveCollToDB(ctx context.Context, s storage.DBAble, coll []model
 
 func (g *Gauge) saveCollToMem(coll []models.Gauge) ([]models.Gauge, error) {
 	for _, rec := range coll {
-		if err := g.storage.Push(models.CounterType, rec.Name, &rec); err != nil {
+		if err := g.storage.Push(models.GaugeType, rec.Name, &rec); err != nil {
 			return nil, err
 		}
 	}
